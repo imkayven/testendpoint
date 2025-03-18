e02c66081e22f96ad299d3249789220fdb56b2f6
iMkayven <62692467+imkayven@users.noreply.github.com>
2025-03-18 23:38:17 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# This function runs before each request
+@app.before_request
+def log_request_info():
+    print(f'Request URL: {request.url}')
+    print(f'Request Method: {request.method}')
+    print(f'Request Headers: {request.headers}')
+    print(f'Request Body: {request.get_data()}')
+
+@app.route('/connect', methods=['POST'])
+def log_post_request():
+    # Get the JSON data from the request
+    data = request.get_json()
+    
+    # Print the data to the console (log)
+    print(data)
+    
+    ###You can also log it to a file if you prefer
+    with open('log.txt', 'a') as f:
+        f.write(str(data) + '\n')
+    
+    # Return a response
+    # return 'Received and logged the POST request successfully!', 200
+    return jsonify(data), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
